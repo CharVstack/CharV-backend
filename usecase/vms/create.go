306386e1b62078a50a69364fe23b1fe01b20bdd8
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2022-10-19 13:55:27 +0900

    Delete types.gen.go

--- usecase/vms/create.go
+++ usecase/vms/create.go
@@ -1,12 +1,12 @@
 package vms
 
 import (
-	"github.com/CharVstack/CharV-backend/adapters"
+	"github.com/CharVstack/CharV-backend/domain/models"
 	"github.com/CharVstack/CharV-lib/pkg/qemu"
 )
 
 // CreateVm diskとVmをcharV-libの関数から作成する
-func CreateVm(vmInfo adapters.PostApiV1VmsJSONRequestBody) (error, error) {
+func CreateVm(vmInfo models.PostApiV1VmsJSONRequestBody) (error, error) {
 	getVmInfo := qemu.InstallOpts{
 		Name:   vmInfo.Name,
 		Memory: vmInfo.Memory,
