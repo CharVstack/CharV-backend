1db2ae2aed0424697bdc3968ac1b70d80901ffd8
bluekey <kameyamatakeru12@gmail.com>
2022-10-20 14:07:59 +0900

    Fix libのディレクトリ構成変更に合わせてリファクタリング

--- usecase/host/cpu.go
+++ usecase/host/cpu.go
@@ -2,10 +2,10 @@ package host
 
 import (
 	"github.com/CharVstack/CharV-backend/domain/models"
-	"github.com/CharVstack/CharV-lib/pkg/host"
+	"github.com/CharVstack/CharV-lib/domain"
 )
 
-func GetCpuInfo(getInfo host.Host) models.Cpu {
+func GetCpuInfo(getInfo domain.Host) models.Cpu {
 	cpuInfo := models.Cpu{
 		Counts:  getInfo.Cpu.Counts,
 		Percent: models.TypeFloat64(getInfo.Cpu.Percent),
