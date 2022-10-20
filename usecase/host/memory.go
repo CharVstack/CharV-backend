dcccf038f615d59645bb0e9fbf620c641475af5c
bluekey <kameyamatakeru12@gmail.com>
2022-10-20 14:07:59 +0900

    Fix libのディレクトリ構成変更に合わせてリファクタリング

--- usecase/host/memory.go
+++ usecase/host/memory.go
@@ -2,10 +2,10 @@ package host
 
 import (
 	"github.com/CharVstack/CharV-backend/domain/models"
-	"github.com/CharVstack/CharV-lib/pkg/host"
+	"github.com/CharVstack/CharV-lib/domain"
 )
 
-func GetMemoryInfo(getInfo host.Host) models.Memory {
+func GetMemoryInfo(getInfo domain.Host) models.Memory {
 	memoryInfo := models.Memory{
 		Total:       models.TypeUint64(getInfo.Memory.Total),
 		Used:        models.TypeUint64(getInfo.Memory.Used),
