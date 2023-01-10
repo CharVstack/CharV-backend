48a32928b3d180b6cc2867acfc137dd0d5d26239
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2023-01-10 10:48:29 +0900

    Socketパスの指定方法を修正

--- pkg/qemu/vnc.go
+++ pkg/qemu/vnc.go
@@ -3,15 +3,17 @@ package qemu
 import (
 	"net/http"
 	"path/filepath"
+	"strings"
 
 	"github.com/gamoutatsumi/go-vncproxy"
 )
 
 func NewVNCProxy(vmId string, logger vncproxy.Logger, socketsDir string) *vncproxy.Proxy {
+	sockPath := append(strings.Split(socketsDir, "/"), "vnc-"+vmId+".sock")
 	return vncproxy.New(&vncproxy.Config{
 		Logger: logger,
 		TokenHandler: func(r *http.Request) (addr, mode string, err error) {
-			return filepath.Join(socketsDir, "vnc-"+vmId+".sock"), "unix", nil
+			return filepath.Join(sockPath...), "unix", nil
 		},
 	})
 }
