2345aa610ac9690dfd7e6b737b25860c3b67c2d9
chaya2z <chaya2zdev@gmail.com>
2023-01-25 12:51:06 +0900

    CVS-168 Fix

--- infrastructure/vnc/vnc.go
+++ infrastructure/vnc/vnc.go
@@ -1,17 +1,18 @@
 package vnc
 
 import (
+	"github.com/CharVstack/CharV-backend/infrastructure/system"
 	"net/http"
 	"path/filepath"
 
 	"github.com/gamoutatsumi/go-vncproxy"
 )
 
-func NewVNCProxy(vmId string, logger vncproxy.Logger, socketsDir string, logLevel uint32) *vncproxy.Proxy {
+func NewVNCProxy(vmId string, logger vncproxy.Logger, path system.Paths, logLevel uint32) *vncproxy.Proxy {
 	return vncproxy.New(&vncproxy.Config{
 		Logger: logger,
 		TokenHandler: func(r *http.Request) (addr, mode string, err error) {
-			return filepath.Join("/tmp/charv/vnc", vmId+".sock"), "unix", nil
+			return filepath.Join(path.VNC, vmId+".sock"), "unix", nil
 		},
 		LogLevel: logLevel,
 	})
